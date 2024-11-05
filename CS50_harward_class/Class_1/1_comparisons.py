a47dc77e5534b7fda0108dcea8422321a1e2bf5d
Benoit-347 <benoitkuriakose@gmail.com>
2024-11-05 22:24:12 +0530

    completed till while

--- CS50_harward_class/Class_1/1_comparisons.py
+++ CS50_harward_class/Class_1/1_comparisons.py
@@ -1,6 +1,6 @@
 #Using decision making, Comparision
 
-#if keyword cointiues block when True
+#if keyword conitiues block when True   #even while keyword does and it is also a conditinal
 #all comparision operators:
 a, b = int(input("enter 1:")), int(input("enter 2:"))
 if a==b:
--- CS50_harward_class/Class_2/1_while.py
+++ CS50_harward_class/Class_2/1_while.py
@@ -1,9 +1,10 @@
-#how to repeat a certain code when a condition is met
+#when condition is met, how to repeat a certain code multiple times
+
 while True: #condition
     print("yes") # executes this block while above is true
     #ctrl c to interupt
-    break #exits all loops when encountered
+    break #exits the current (inner most/nested most/latest/most subset) loop when encountered
 i = 0
 while i == 0:
     print("a is zero")
-    i = i + 1 #unlike for, can change state of condition when it repeates
+    i = i + 1 #unlike for, body can change state of condition when it repeates
