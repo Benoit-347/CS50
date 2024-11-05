a1d2368d50b35e7feba90dda8dd99d008daa98e5
Benoit-3 <oneriver11112@gmail.com>
2024-11-05 00:23:53 -0800

    Update 5.py

--- CS50_harward_class/CLass_7/5.py
+++ CS50_harward_class/CLass_7/5.py
@@ -10,15 +10,34 @@ def a():
     else:
         print("no")
 
-#want to set number of repititions?
+#want to set number of repititions of character before/ after str?
 # "*" = 0 or more
 # "+" = 1 or more
 # "?" = 0 or 1
 # "{m}"" = m rep
 # "{m,n}"" = m to n rep
 
-a = input("enter: ")
-if re.search(".+@", a):     #first arg = ".<repeat type><pattern>"
-    print("yes")
-else:
-    print("no")
+def charac_before():
+    a = input("enter: ")
+    if re.search(".+@.+", a):     #first arg = ".<repeat type><pattern>.<repeat type>"
+        print("yes")
+    else:
+        print("no")
+
+def email_check0():
+    a = input("enter: ")
+    if re.search(".+@.+..+", a):     #does not read . as "."
+        print("yes")
+    else:
+        print("no")
+
+email_check0()
+
+def email_check():
+    a = input("enter: ")
+    if re.search(r".+@.+\..+", a):     #using backslash to read "."     #need to put as raw string ("r") to tell py to not intepret "\" as usual way (\n or beggining and ending escape seq)
+        print("yes")
+    else:
+        print("no")
+
+email_check()
